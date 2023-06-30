63959bfbaae7df3396badd1a0f68be045919c129
Michael Tanaka <tanakam4214@gmail.com>
2023-06-29 23:46:07 -0700

    updated empty muscle group display

--- src/components/WorkoutsPage.js
+++ src/components/WorkoutsPage.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import WorkoutCard from "./WorkoutCard";
 import { v4 as uuid } from "uuid";
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, Segment, Header, Icon } from "semantic-ui-react";
 import { WorkoutContext } from "../context/WorkoutContextProvider";
 import { Link } from "react-router-dom";
 
@@ -26,15 +26,20 @@ function WorkoutsPage() {
           <Card.Group itemsPerRow="3">{workoutGroups[group]}</Card.Group>
           {activeItem.toLowerCase() === group.toLowerCase() && workoutGroups[group].length === 0 ? (
             <>
-              <h1 style={{ textAlign: "center" }}>There are no {group.toLowerCase()} workouts saved.</h1>
-              <Button
-                as={Link}
-                to={"/add-workout"}
-                color="green"
-                style={{ maxWidth: "20%", display: "block", margin: "0 auto" }}
-              >
-                Add Workout
-              </Button>
+              <Segment placeholder style={{ marginTop: "60px" }}>
+                <Header icon>
+                  <Icon name="ban" />
+                  There are no {group.toLowerCase()} workouts saved.
+                </Header>
+                <Button
+                  as={Link}
+                  to={"/add-workout"}
+                  color="green"
+                  style={{ maxWidth: "20%", display: "block", margin: "0 auto" }}
+                >
+                  Add Workout
+                </Button>
+              </Segment>
             </>
           ) : null}
         </div>
